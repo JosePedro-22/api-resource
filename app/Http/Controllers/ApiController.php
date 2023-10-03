9d3bcd0310401498c5d5d371488d3e9abd7200b3
Jose Pedro <jose.mesquita@seati.ma.gov.br>
2023-10-03 16:20:29 -0300

    finish introduction content api-resource

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,10 +9,8 @@ class ApiController extends Controller
 {
     public function index(){
         $users = User::query()
-            ->when(request()->header('show')==='all',
-            fn($query) => $query->with('subject'))
             ->get();
-        
+
         return UserResource::collection($users);
     }
 }
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -8,19 +8,31 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
+    private function showData($request, $value){
+        if(!$request->headers->get('columns')) return true;
+
+        return str($request->headers->get('columns'))->contains($value);
+    }
+
     public function toArray(Request $request): array
     {
-        dd($request);
         $show = $request->headers->get('show') === 'all';
+
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
+            'id' => $this->when(
+                $this->showData($request, 'id'),
+                $this->id),
+            'name' => $this->when(
+                $this->showData($request, 'name'),
+                $this->name),
+            'email' => $this->when(
+                $this->showData($request, 'email'),
+                $this->email),
             $this->mergeWhen($show, [
                 'created_at' => $this->created_at,
-                'updated_at' => $this->updated_at,  
-                'subjects' => $this->whenLoaded('subject'),
-            ]),            
+                'updated_at' => $this->updated_at,
+                'subjects' => $this->subject->name,
+            ]),
         ];
     }
 }
